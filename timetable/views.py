1d6e9df7b9d8371ae12d7d61c32a599acf787da0
gabyantova <gaby.antova@gmail.com>
2016-12-02 10:06:17 +0000

    restoring prev changes

--- timetable/views.py
+++ timetable/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
-import urllib.request
+import urllib
 import datetime
 
 # Create your views here.
@@ -21,8 +21,8 @@ def get_url():
     return "http://web.inf.ed.ac.uk/infweb/student-services/ito/admin/timetables/lecture-timetable-20" + str(ac_year_start) + "-" + str(ac_year_start + 1)
 
 
-soup = BeautifulSoup(urllib.request.urlopen(get_url()).read())
-	
+#soup = BeautifulSoup(urllib.request.urlopen(get_url()).read())
+soup = BeautifulSoup(urllib.urlopen(get_url()).read())
 times = soup.table.thead.tr.find_all("th")
 
 
